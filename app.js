54f0684ad0be87925877898f9cac1b4d172c3fbd
Angele-NG <angelenguiakam@gmail.com>
2022-11-23 10:40:45 +0100

    angele

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 let current_music = document.createElement('audio');
 
-function load_music()
-{
+function load_music() {
     current_music.src = "musics/Mayole.mp3";
     console.log(current_music.src);
     current_music.load();
     current_music.play();
 }
 
-function hee_vampire()
-{
+function hee_vampire() {
     console.log("La fonction s'est finale;ent lancee");
 }
 
-function random_bg_color(){
+function random_bg_color() {
     let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
     let a;
     console.log("La fonction a ete lancee");
 
-    function populate(a){
-        for(let i=0; i<6; i++){
+    function populate(a) {
+        for (let i = 0; i < 6; i++) {
             let x = Math.round(Math.random() * 14);
             let y = hex[x];
             a += y;
@@ -33,3 +31,19 @@ function random_bg_color(){
     let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
     document.body.style.background = gradient;
 }
+
+let colorPicker = document.getElementById("colorPicker");
+let box = document.getElementById("box");
+let output;
+
+output = document.getElementById("output");
+
+box.style.borderColor = colorPicker.value;
+
+colorPicker.addEventListener("input", function(event) {
+    box.style.borderColor = event.target.value;
+}, false);
+
+colorPicker.addEventListener("change", function(event) {
+    output.innerText = "Couleur choisie : " + colorPicker.value;
+}, false);
